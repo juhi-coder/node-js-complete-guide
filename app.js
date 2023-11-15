b0ac5e11aa8db8b9b1e437955a125af981094502
juhi-coder <kumarijuhimdb2@gmail.com>
2023-11-15 15:00:29 +0530

    Simple Group Chat Application

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const fs = require("fs");
 const app = express();
 
 const adminRoutes = require("/routes/admin");
 const shopRoutes = require("./routes/shop");
+const loginRoutes = require("./login");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+appuse(cookieParser());
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(loginRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page Not Found</h1>");
